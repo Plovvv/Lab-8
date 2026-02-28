ee030e70a23d9da0b2bdf8049981d849dd65488a
Fedor <fedor.radzhapov34505@gmail.com>
2026-03-01 00:59:46 +0300

    answer

--- main.py
+++ main.py
@@ -1,69 +1,93 @@
-import time
-
-import cv2
-
-
-def image_processing():
-    img = cv2.imread('img_test.jpg')
-    #cv2.imshow('image', img)
-    w, h = img.shape[:2]
-    #(cX, cY) = (w // 2, h // 2)
-    #M = cv2.getRotationMatrix2D((cX, cY), 45, 1.0)
-    #rotated = cv2.warpAffine(img, M, (w, h))
-    #cv2.imshow('rotated', rotated)
-
-    #cat = img[250:580, 20:280]
-    #cv2.imshow('image', cat)
-
-    #r = cv2.selectROI(img)
-    #image_cropped = img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
-    #cv2.imshow('cropped', image_cropped)
-
-    cv2.line(img, (0, 0), (580, 600), (255, 0, 0), 5)
-    cv2.rectangle(img, (384, 10), (580, 128), (0, 252, 0), 3)
-    cv2.putText(img, 'Lab. No 8', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 3,
-                (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.imshow('img', img)
+import cv2 as cv
+import numpy as np
+import sys
 
 
-def video_processing():
-    cap = cv2.VideoCapture(1)
-    down_points = (640, 480)
-    i = 0
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+#1
+# img = cv.imread("images\\variant-4.jpeg")
+ 
+# if img is None:
+#     sys.exit("Could not read the image.")
 
-        frame = cv2.resize(frame, down_points, interpolation=cv2.INTER_LINEAR)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        ret, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY_INV)
+# blue = cv.split(img)[0]
+# zeros = np.zeros(blue.shape, np.uint8)
 
-        contours, hierarchy = cv2.findContours(thresh,
-                            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        if len(contours) > 0:
-            c = max(contours, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            if i % 5 == 0:
-                a = x + (w // 2)
-                b = y + (h // 2)
-                print(a, b)
-
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        time.sleep(0.1)
-        i += 1
-
-    cap.release()
+# blue_colored = cv.merge([blue, zeros, zeros])
+# cv.imshow('Blue Color Only', blue_colored)
+# k = cv.waitKey(0)
 
 
-if __name__ == '__main__':
-    #image_processing()
-    video_processing()
+#2, 3
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img_fly = cv.imread("fly64.png")
+if img_fly is None:
+    sys.exit("Could not read the image.")
+
+while True:
+    h_f, w_f, _ = img_fly.shape
+
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    cv.rectangle(
+        frame,
+        (frame.shape[1]//2, 0),
+        (frame.shape[1], frame.shape[0]),
+        (255,0,0),
+        1
+        )
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv.GaussianBlur(gray, (9,9), 2)
+
+    circles = cv.HoughCircles(
+        gray,
+        cv.HOUGH_GRADIENT,
+        dp=1,
+        minDist=20,
+        param1=50,
+        param2=35,
+        minRadius=50,
+        maxRadius=300)
+    
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+
+        for x, y, r in circles[0,:]:
+            if y-r < 0 or y+r > frame.shape[0] or x-r < 0 or x+r > frame.shape[1]:
+                continue
+
+            roi = gray[y-r:y+r, x-r:x+r]
+            if roi.size == 0:
+                continue
+            roi = cv.resize(roi, (200,200))
+
+            _, roi_bin = cv.threshold(roi, 127, 255, cv.THRESH_BINARY)
+
+            white_ratio = np.sum(roi_bin == 255) / (200*200)
+
+            if 0.35 < white_ratio < 0.65: 
+                for i in circles[0,:]:
+                    cv.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+                    # cv.circle(frame,(i[0],i[1]),2,(0,0,255),3)
+                    frame[i[1]-h_f//2:i[1]+h_f//2, i[0]-w_f//2:i[0]+w_f//2] = img_fly
+
+                    break
+            
+            if x >= frame.shape[1]/2:
+                font = cv.FONT_HERSHEY_SIMPLEX
+                cv.putText(frame,'Right',(50,frame.shape[0]-10), font, 1,(255,255,255),2,cv.LINE_AA)
+    
+    cv.imshow('detected circles',frame)
+    if cv.waitKey(1) & 0xFF == 27:
+        break
+
+
+cv.destroyAllWindows()
